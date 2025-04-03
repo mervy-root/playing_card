ad20e5a2b9033569ccdcf5a4319decc33f894fa8
mervy-root <tsafackmerveille15@gmail.com>
2025-04-03 17:02:17 +0000

    first part of output done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,28 @@
 import { Component } from '@angular/core';
 import { PlayingCardComponent } from "./playing-card/playing-card.component";
 import { Monster } from './models/monster.model';
+import { SearchBarComponent } from "./search-bar/search-bar.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  //imports: [PlayingCardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: [PlayingCardComponent]
+  imports: [PlayingCardComponent, SearchBarComponent]
 })
 
 export class AppComponent {
   monster1!: Monster; //on ne l initialise pas tout de suite et on s assure qu elle ne soit pas nulle
-
+  count: number = 0;    //compter le nombre de fois que la recherche est effectue
+  
   constructor(){
     this.monster1 = new Monster();
     this.monster1.name = "Pik";
     this.monster1.hp = 40;
     this.monster1.figureCaption = "N 002 Pik";
+  }
+
+  increaseCount(){
+    this.count++;
   }
 }
---
+++ src/app/search-bar/search-bar.component.ts
@@ -0,0 +1,16 @@
+import { Component, EventEmitter, Output } from '@angular/core';
+import { count } from 'rxjs';
+
+@Component({
+  selector: 'app-search-bar',
+  imports: [],
+  templateUrl: './search-bar.component.html',
+  styleUrl: './search-bar.component.css'
+})
+export class SearchBarComponent {
+
+  @Output() searchButtonClicked = new EventEmitter();
+  searchClick(){
+    this.searchButtonClicked.emit();
+  }
+}
